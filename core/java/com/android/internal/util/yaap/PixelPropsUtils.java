29ca51661a9b0b4cf488d7a249f8f2c6b9f15bff
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-03-20 19:23:05 +0800

    PixelPropsUtils: Spoof Google Photos to Redfin
    In QPR2 something broke unlimited quality photos backup. But unlimited compressed backups still work fine
    Spoof photos to redfin and whitelist XMLs based on Pixelify Google Photos module

--- core/java/com/android/internal/util/yaap/PixelPropsUtils.java
+++ core/java/com/android/internal/util/yaap/PixelPropsUtils.java
@@ -136,7 +136,6 @@ public final class PixelPropsUtils {
     ));
 
     private static final HashSet<String> marlinPackagesToChange = new HashSet<>(Set.of(
-        "com.google.android.apps.photos",
         "com.samsung.accessory.berrymgr",
         "com.samsung.accessory.fridaymgr",
         "com.samsung.accessory.neobeanmg",
@@ -149,6 +148,7 @@ public final class PixelPropsUtils {
 
     private static final HashSet<String> redfinPackagesToChange = new HashSet<>(Set.of(
         "com.google.android.googlequicksearchbox",
+        "com.google.android.apps.photos",
         "com.google.android.apps.recorder"
     ));
 
